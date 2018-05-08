85b5db4eb1530e00cdf0a13b68457daed4a4b6b7
yanyan.sun <yanyan.sun@weimob.com>
2018-05-08 18:26:28 +0800

    Highcharts.setOptions 去掉商标

--- src/components/Highcharts.vue
+++ src/components/Highcharts.vue
@@ -17,6 +17,9 @@ export default {
   },
   methods: {
       initChart() {
+          Highcharts.setOptions({
+              credits: { enabled: false }
+          })
           this.$el.style.width = (this.styles.width || 400) + 'px'
           this.$el.style.height = (this.styles.height || 300) + 'px'
           this.chart = new Highcharts.Chart(this.$el, this.options)
